b66eb3716037cc35adb0689b1c5b1690e0830251
Nick <nconkl1@outlook.com>
2022-10-28 19:38:39 -0400

    added convert_time function

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,4 +1,7 @@
 use owo_colors::OwoColorize;
+use time::Time;
+use time::error::Parse;
+use time::format_description;
 
 pub fn print_header() {
     let s = r#"
@@ -11,3 +14,14 @@ pub fn print_header() {
     "#.red();
     println!("{}", s);
 }
+
+pub fn convert_time(entered_time: &str) -> f64 {
+    let format = format_description::parse("[hour]:[minute]").unwrap();
+    let time = Time::parse(entered_time, &format).expect("Incorrect time format");
+    
+    let minutes = time.minute() as f64;
+    let decimal_minutes: f64 = minutes / 60.0;
+
+    decimal_minutes
+}
+
