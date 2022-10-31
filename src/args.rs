403d823223914dd215ea861d625ce3944b6932b1
Nick Conklin <nconkl1@outlook.com>
2022-10-31 10:57:38 -0400

    formatted Time struct

--- src/args.rs
+++ src/args.rs
@@ -32,6 +32,6 @@ pub enum Commands {
 #[derive(Args)]
 pub struct Time {
     /// The times to convert (multiple times separated by a comma)
-    #[arg(short, long, value_delimiter=',')]
+    #[arg(short, long, value_delimiter = ',')]
     pub times: Vec<String>,
 }
