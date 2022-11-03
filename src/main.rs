a64212904dcc82fe3dafc781709bd65b9119be9d
Nick <nconkl1@outlook.com>
2022-11-03 17:13:31 -0400

    testing things

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
 mod args;
 mod helpers;
 
-use core::panic;
-
 use args::Cli;
+use args::Commands;
 use clap::Parser;
+use helpers::print_header;
+use helpers::read_file;
+use helpers::{pretty_print_results, convert_times};
 fn main() {
     let args = Cli::parse();
 
-    helpers::print_header();
-    let times = helpers::convert_times(vec!["12:05".to_string(), "12:10".to_string()]);
-
-    match times {
-        Ok(times) => println!("{:.2?}", times),
-        Err(_) => panic!("Error"),
+    match args.quiet {
+        true => {},
+        false => print_header(&args),
     }
+
+    match args.command {
+        Commands::Time(options) => {
+            match options.times {
+                Some(times) => {
+                    let converted_times = convert_times(times);
+                    pretty_print_results(converted_times);
+                },
+                None => println!("No times detected. Please enter some times")
+            };
+            match options.file {
+                Some(path_to_file) => {
+                    let converted_times = read_file(path_to_file);
+                    pretty_print_results(converted_times);
+                },
+                None => println!("No file path detected.")
+            }
+        }
+    }
+
 }
