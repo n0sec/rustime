ddcea50b0efedd9fcc21bc2f32dfa9303f85e7fa
Nick Conklin <nconkl1@outlook.com>
2022-11-02 20:55:49 -0400

    modified read_file to return tuple

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,11 +1,11 @@
-use std::fs::File;
-use std::io::Write;
 use chrono::Utc;
+use comfy_table::Table;
 use comfy_table::*;
 use owo_colors::{OwoColorize, Style};
+use std::fs::File;
+use std::io::Write;
 use time::format_description;
 use time::Time;
-use comfy_table::Table;
 
 use crate::args::Cli;
 
@@ -39,14 +39,13 @@ pub fn print_header(args: &Cli) {
         true => {
             println!("{}", s);
             println!("{}", info);
-        },
+        }
         false => {
             println!("{}", s.style(title_style));
             println!("{}", info.style(info_style));
-        },
+        }
     }
 }
-
 
 pub fn convert_times(entered_times: Vec<String>) -> Result<Vec<f64>, time::error::Parse> {
     let format = format_description::parse("[hour]:[minute]")
@@ -55,20 +54,26 @@ pub fn convert_times(entered_times: Vec<String>) -> Result<Vec<f64>, time::error
     entered_times
         .iter()
         .map(|time| Time::parse(time, &format))
-        .map(|maybe_parsed_time| maybe_parsed_time.map(|time| (time.minute() as f64 / 60.0) + time.hour() as f64))
+        .map(|maybe_parsed_time| {
+            maybe_parsed_time.map(|time| (time.minute() as f64 / 60.0) + time.hour() as f64)
+        })
         .collect::<Result<Vec<_>, time::error::Parse>>()
 }
 
-// TODO: Needs to also return the entered times lol
-pub fn read_file(path_to_file: String) -> Result<Vec<f64>, ReadTimesError> {
+pub fn read_file(path_to_file: String) -> Result<Vec<(String, f64)>, ReadTimesError> {
     let format = format_description::parse("[hour]:[minute]")
         .expect("Programming error: Invalid time formatter.");
 
     let file = std::fs::read_to_string(path_to_file)?;
 
     file.lines()
-        .map(|time| Time::parse(time, &format).map_err(ReadTimesError::from))
-        .map(|maybe_parsed_time| maybe_parsed_time.map(|time| time.minute() as f64 / 60.0))
+        .map(|time| {
+            (
+                time,
+                Time::parse(time, &format).map_err(ReadTimesError::from),
+            )
+        })
+        .map(|(time, parsed)| parsed.map(|parsed| (time.to_owned(), parsed.minute() as f64 / 60.0)))
         .collect::<Result<Vec<_>, ReadTimesError>>()
 }
 
@@ -82,8 +87,8 @@ pub fn write_file(entered_times: Vec<String>) -> Result<File, ReadTimesError> {
 
     // Write to the file
     for time in entered_times {
-        file.write(time.as_bytes())?;
-    };
+        file.write_all(time.as_bytes())?;
+    }
 
     // Return the file
     Ok(file)
@@ -95,7 +100,14 @@ pub fn pretty_print_results(results: Vec<f64>) -> () {
     let mut table = Table::new();
 
     // Set the header before adding rows
-    table.set_header(vec![Cell::new("Times").fg(Color::Blue).add_attribute(Attribute::Bold), Cell::new("Converted Output").fg(Color::Blue).add_attribute(Attribute::Bold)]);
+    table.set_header(vec![
+        Cell::new("Times")
+            .fg(Color::Blue)
+            .add_attribute(Attribute::Bold),
+        Cell::new("Converted Output")
+            .fg(Color::Blue)
+            .add_attribute(Attribute::Bold),
+    ]);
 
     // Loop over the Vector of results and create rows of the times and converted output
     for result in results {
@@ -105,5 +117,3 @@ pub fn pretty_print_results(results: Vec<f64>) -> () {
     // Print the table
     println!("{table}");
 }
-
-
