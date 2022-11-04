27dde6ec07dcb3fc3ee1fe9ea915712aa3cfccac
Nick <nconkl1@outlook.com>
2022-11-04 15:12:31 -0400

    removed subcommand for one struct

--- src/args.rs
+++ src/args.rs
@@ -1,15 +1,19 @@
-use clap::{Args, Parser, Subcommand};
+use clap::{Parser};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about=None)]
-pub struct Cli {
+pub struct CliOptions {
+    /// The times to convert written in hh:mm format (multiple times separated by a comma)
+    #[arg(short, long, value_delimiter = ',', conflicts_with="file", required_unless_present="file", group="time")]
+    pub times: Vec<String>,
+
+    /// The input file to read and parse
+    #[arg(short, long, group="input")]
+    pub file: Option<String>,
+
     /// Don't print the banner and other noise
     #[arg(short, long, global = true, help_heading = "Flags")]
     pub quiet: bool,
-
-    /// Disable color output
-    #[arg(long, global = true, help_heading = "Flags")]
-    pub no_color: bool,
 
     /// Don't display progress
     #[arg(short = 'z', long, global = true, help_heading = "Flags")]
@@ -18,24 +22,4 @@ pub struct Cli {
     /// Output file to write results to
     #[arg(short, long, global = true, help_heading = "Flags")]
     pub output: Option<String>,
-
-    #[command(subcommand)]
-    pub command: Commands,
-}
-
-#[derive(Subcommand, Debug)]
-pub enum Commands {
-    /// Parses times into decimal
-    Time(TimeOptions),
-}
-
-#[derive(Args, Debug)]
-pub struct TimeOptions {
-    /// The times to convert (multiple times separated by a comma)
-    #[arg(short, long, value_delimiter = ',', conflicts_with="file", group="time")]
-    pub times: Option<Vec<String>>,
-
-    /// The input file to read and parse
-    #[arg(short, long, group="input")]
-    pub file: Option<String>,
 }
